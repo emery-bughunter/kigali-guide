1d6a85b9a4f95075195c97e221dfb34631cf7e7a
Emery Barame <e.barame@alustudent.com>
2026-03-08 23:50:05 +0200

    minsdk

--- assignment/android/app/build.gradle.kts
+++ assignment/android/app/build.gradle.kts
@@ -26,7 +26,7 @@ android {
         applicationId = "com.example.assignment"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = 21  // geolocator requires API 21+
+        minSdk = flutter.minSdkVersion  // geolocator requires API 21+
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
